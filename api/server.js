363d2819b27563dd40fa29aea72f85f6aeacc134
Daniel Terry <daniel.terry2121@gmail.com>
2021-05-01 13:10:04 -0400

    MVP

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const carRouter = require('../cars/cars-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api/cars',carRouter);
+server.get('/',(req,res)=>{
+    res.status(200).json({api: "up"})
+})
+
+module.exports = server;
